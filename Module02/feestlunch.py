81e71c34e9e3f484d33d365d092f612fb9aca696
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-09-08 13:52:47 +0200

    Add files via upload

---
+++ Module02/feestlunch.py
@@ -0,0 +1,14 @@
+croissantpp = 0.39
+stokbroodpp = 2.78
+kortingsbon = 0.50
+totaal = croissantpp * 17+stokbroodpp * 2-kortingsbon * 3
+
+print('Berekening kosten')
+print(' ')
+print(f'17 Croissantjes kosten { croissantpp * 17 } Euro')
+print(f'2 Stokbrooden kosten { stokbroodpp * 2 } Euro')
+print(' ')
+print(f'Totaal bespaard geld met de Kortingsbonnen { kortingsbon * 3 } Euro')
+print(' ')
+print(f'De feestlunch kost je bij de bakker 10.69 euro voor de 17 croissantjes en de 2 stokbroden')
+print(f' als de 3 kortingsbonnen nog geldig zijn!')
---
+++ Module02/speelhal.py
@@ -0,0 +1,9 @@
+ticketprice = 7.45
+VRperfivemin = 0.37
+totaal = ticketprice * 3 + VRperfivemin * 9 * 3
+
+print('Totaal Kosten uitje')
+print(f'Toegang koste zijn { ticketprice * 3 } Euro')
+print(f'VIP-VR-GameSeat koste zijn { VRperfivemin * 9 * 3 } Euro')
+print('')
+print(f'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {totaal} euro')
