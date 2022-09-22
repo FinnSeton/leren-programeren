ae1567884145b00a2a36111a8506bc0fc5aaca71
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-09-22 10:17:45 +0200

    Add files via upload

---
+++ challenges/smartphone.py
@@ -0,0 +1,18 @@
+iphone13 = int(input("Prijs Iphone 13: "))
+SamsungGS22 = int(input("Prijs Samsung Galaxy S22: "))
+berekening = iphone13 - SamsungGS22
+print("")
+if iphone13 > SamsungGS22:
+    print(f"De Iphone 13 is het duurst, de telefoon kost:{iphone13}")
+    print(
+        f"De Samsung Galaxy S22 is het goedkoopst, de telefoon kost: {SamsungGS22} Euro")
+elif iphone13 < SamsungGS22:
+    print(
+        f"De Samsung Galaxy S22 is het duurst, de telefoon kost:{SamsungGS22} Euro ")
+    print(f"De Iphone 13 is het goedkoopst, de telefoon kost: {iphone13} Euro")
+if berekening > 50:
+    print(
+        f"Het advies is dus de Samsung Galaxy S22 te kopen. Deze is namelijk {berekening} goedkoper / duurder dan de Iphone 13")
+else:
+    print(
+        f"Het advies is dus de Iphone 13 te kopen. Deze is namelijk {berekening} euro goedkoper / duurder dan de Samsung Galaxy S22")
