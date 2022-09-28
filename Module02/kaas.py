2e2bc6ae205e33f1d6a2cc27f306e641fbac8082
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-09-28 10:03:45 +0200

    Update kaas.py

--- Module02/kaas.py
+++ Module02/kaas.py
@@ -1,14 +1,14 @@
-answer = input("Is de kaas geel?: ") 
+answer = input("Is de kaas geel?: ").lower 
 if answer == "ja": 
-     answer = input("Zitten ergaten in?: ") 
+     answer = input("Zitten ergaten in?: ").lower 
      if answer == "ja": 
-         answer = input("Is de kaas belagelijk duur?: ") 
+         answer = input("Is de kaas belagelijk duur?: ").lower 
          if answer == "ja": 
           print("Emmenthaler")
          elif answer == "nee": 
           print("Leerdammer")
      elif answer == "nee": 
-         answer = input("Is de kaas  hard als steen?: ") 
+         answer = input("Is de kaas  hard als steen?: ").lower 
          if answer == "ja": 
           print("Parmigiano Reggiano")
          elif answer == "nee": 
@@ -16,15 +16,15 @@ if answer == "ja":
 
 
 elif answer == "nee": 
- answer = input("Heeft de kaasblauweschimmel?: ") 
+ answer = input("Heeft de kaasblauweschimmel?: ").lower  
  if answer == "ja": 
-         answer = input("Heeft de kaas een korst?: ") 
+         answer = input("Heeft de kaas een korst?: ").lower  
          if answer == "ja": 
           print("Blue de Rochbaron")
          elif answer == "nee": 
           print("Foume d'Ambert")
  elif answer == "nee": 
-         answer = input("Heeft de kaaseen korst?: ") 
+         answer = input("Heeft de kaaseen korst?: ").lower  
          if answer == "ja": 
           print("Camembert")
          elif answer == "nee": 
