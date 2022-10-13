31ca693e250970e1877f4eb7e4c8ea68be3f0836
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-10-13 09:38:14 +0200

    Update Zwembad.py

--- challenges/Zwembad.py
+++ challenges/Zwembad.py
@@ -1,1 +1,15 @@
-print('36')
+uitgraven = 25 #per m3
+afvoeren = 32,50 #per m3
+lengte = 8
+breedte = 3
+diepte = 1,5
+m3 = lengte * breedte * diepte
+uittot = uitgraven * m3
+aftot = afvoeren * m3
+tot = uittot + aftot
+
+print(f'Offerte voor een zwembad van 8 bij 3 bij 1,5 meter (inhoud: {m3} m3)')
+
+print(f'Uitgraven: {uittot} euro')
+print(f'Afvoeren grond: {aftot} euro')
+print(f'Totaal: {tot} euro')
