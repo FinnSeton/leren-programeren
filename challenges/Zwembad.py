d324deecc236b0bc54ae1765e64085c8dc99f292
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-10-13 09:53:24 +0200

    Update Zwembad.py

--- challenges/Zwembad.py
+++ challenges/Zwembad.py
@@ -1,15 +1,26 @@
 uitgraven = 25 #per m3
-afvoeren = 32,50 #per m3
+afvoeren = 32.50 #per m3
+km = 60
 lengte = 8
 breedte = 3
-diepte = 1,5
+diepte = 1.5
 m3 = lengte * breedte * diepte
 uittot = uitgraven * m3
 aftot = afvoeren * m3
 tot = uittot + aftot
 
-print(f'Offerte voor een zwembad van 8 bij 3 bij 1,5 meter (inhoud: {m3} m3)')
+if m3 <= 20 and km <= 50:
+    voorrijkosten = km * 1.25 + 100
+elif m3 <= 20 and km >= 50:
+    voorijkosten = km * 1.15 + 100
+elif m3 >= 20 and km <= 50:
+    voorijkosten = km * 2.15 + 250
+elif m3 >= 20 and km >= 50:
+    voorijkosten = km * 2.05 + 250
 
+
+print(f'Offerte voor een zwembad van 8 bij 3 bij 1,5 meter (inhoud: {m3} m3)')
 print(f'Uitgraven: {uittot} euro')
 print(f'Afvoeren grond: {aftot} euro')
+print(f'Voorrijkosten: {voorijkosten} euro')
 print(f'Totaal: {tot} euro')
