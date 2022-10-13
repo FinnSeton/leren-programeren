8379a5d26dc6ef77db4682bc429e2f93cf30d7c6
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-10-13 10:09:19 +0200

    Update Zwembad.py

--- challenges/Zwembad.py
+++ challenges/Zwembad.py
@@ -1,10 +1,15 @@
 uitgraven = 25 #per m3
 afvoeren = 32.50 #per m3
 km = 60
-lengte = input('Voer de lengte in van je zwembad')
-breedte = input('Voer de breedte in van je zwembad')
-diepte = input('Voer de diepte/hoogte in van je zwembads')
+try:
+    lengte = input(int('Voer de lengte in van je zwembad'))
+    breedte = input(int('Voer de breedte in van je zwembad'))
+    diepte = input(int('Voer de diepte/hoogte in van je zwembads'))
+    tegeloptie = input('Wil u uw tegel Rood/Standaard/Zelf kiezen.').lower()
+except:
+    print('Verkeerde input iets is fout gegaan')
 m3 = lengte * breedte * diepte
+m2 = lengte * breedte
 uittot = uitgraven * m3
 aftot = afvoeren * m3
 tot = uittot + aftot
@@ -18,9 +23,28 @@ elif m3 >= 20 and km <= 50:
 elif m3 >= 20 and km >= 50:
     voorijkosten = km * 2.05 + 250
 
+if m3 <= 20:
+    tegelm2 = 250
+    if tegeloptie == 'standaard':
+        tegelmeerpijs = 0
+    elif tegeloptie == 'zelf kiezen':
+        tegelmeerpijs = 100
+    elif tegeloptie == 'rood':
+        tegelmeerpijs = 25 * m2
+elif m3 >= 20:
+    tegelm2 = 200
+    if tegeloptie == 'standaard':
+        tegelmeerpijs = 0
+    elif tegeloptie == 'zelf kiezen':
+        tegelmeerpijs = 125
+    elif tegeloptie == 'rood':
+        tegelmeerpijs = 20 * m2
+Tegeltot = m2 * tegelm2 + tegelmeerpijs
+
 
 print(f'Offerte voor een zwembad van 8 bij 3 bij 1,5 meter (inhoud: {m3} m3)')
 print(f'Uitgraven: {uittot} euro')
 print(f'Afvoeren grond: {aftot} euro')
 print(f'Voorrijkosten: {voorijkosten} euro')
+print(f'Beton + Tegels: {Tegeltot} euro')
 print(f'Totaal: {tot} euro')
