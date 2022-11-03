f57b7477915820b4838fe3c548d3db70e20f1d5e
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-11-03 09:37:28 +0100

    Update tijd while.py

--- Module03/tijd while.py
+++ Module03/tijd while.py
@@ -1,18 +1,11 @@
 uur = 0
-min = 0
-while uur <= 11:
-    if min == 60:
-        uur = uur + 1
-        min = 0
-    min = min + 1
-    ap = ('AM')
-    print (ap, uur, min)
-else:
-    while uur <= 23:
-        if min == 60:
-            uur = uur + 1
-            min = 0
-        min = min + 1
-        ap = ('PM')
-        print (ap, uur, min)
+ap = 'AM'	
+while uur <= 24:
+    print (ap, uur, '00')
+    if uur <= 12:
+        ap = 'AM'
+    if uur >= 12:
+        ap = 'PM'
+    uur = uur + 1 
+        
     
