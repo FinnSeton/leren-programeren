6434e50e538980fb60bfb8daf4c1cb4395eef756
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-12-06 10:23:35 +0100

    Update raadeens.py

--- Module03/raadeens.py
+++ Module03/raadeens.py
@@ -1,38 +1,39 @@
 import random
 
 rounds = 0
-tries = 0
 userInput = ""
 points = 0
 
-while rounds < 20 and userInput != "quit":
+
+while rounds < 3 and userInput != "quit":
     rounds += 1
+    tries = 0
+    geraden = False
     print(f"rounds: {rounds}")
     randomNum = random.randint(1, 1000)
-    for i in range(1,10):
-        print(f"\ntries: {i}")
+    print(randomNum)
+    while tries <= 3 and userInput != "quit" and geraden == False :
+        print(f"\ntries: {tries}")
         userInput = input("enter a whole number between 1-1000: ")
-        if userInput == "quit":
-            break
-        else:
+        tries += 1
+        if userInput != "quit":
             userInput = int(userInput)
-
-        difference = abs(randomNum - userInput)
-        if randomNum == userInput:
-            print("correct!")
-            points += 1
-            break
-        elif difference < 20 and randomNum < userInput:
-            print("wrong, you're very warm, try smaller")
-        elif difference < 20 and randomNum > userInput:
-            print("wrong, you're very warm, try bigger")
-        elif difference < 50 and randomNum < userInput:
-            print("wrong, you're warm, try smaller")
-        elif difference < 50 and randomNum > userInput:
-            print("wrong, you're warm, try bigger")
-        elif randomNum < userInput:
-            print("Wrong, try smaller")
-        else:
-            print("Wrong, try bigger")
+            difference = abs(randomNum - userInput)
+            if randomNum == userInput:
+                print("correct!")
+                points += 1
+                geraden = True
+            elif difference < 20 and randomNum < userInput:
+                print("wrong, you're very warm, try smaller")
+            elif difference < 20 and randomNum > userInput:
+                print("wrong, you're very warm, try bigger")
+            elif difference < 50 and randomNum < userInput:
+                print("wrong, you're warm, try smaller")
+            elif difference < 50 and randomNum > userInput:
+                print("wrong, you're warm, try bigger")
+            elif randomNum < userInput:
+                print("Wrong, try smaller")
+            else:
+                print("Wrong, try bigger")
 
 print(f"\npoints: {points}")
