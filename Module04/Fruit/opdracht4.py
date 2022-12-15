db97b4dc0f5ccf877e596fe32f0a354099d058ea
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-12-15 12:12:24 +0100

    Create opdracht4.py

---
+++ Module04/Fruit/opdracht4.py
@@ -0,0 +1,11 @@
+from fruitmand import fruitmand
+import random
+a = -1
+user_input = int(input("Hoeveel: "))
+
+for i in fruitmand:
+    a = a + 1
+
+for i in range(0, user_input):
+    random_fruit = random.randint(0, a)
+    print(fruitmand[random_fruit]['name'])
