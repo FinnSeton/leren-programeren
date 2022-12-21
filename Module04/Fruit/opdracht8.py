ba480b560553683925b06d730db1423d0ec3a20e
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-12-21 12:01:13 +0100

    Create opdracht8.py

---
+++ Module04/Fruit/opdracht8.py
@@ -0,0 +1,8 @@
+from fruitmand import fruitmand
+a = 0
+fruitmand.append({'name': 'watermeloen', 'weight': 2000, 'color': 'green', 'round': True})
+
+for i in fruitmand:
+    b = i['weight']
+    a += b
+print(a)
