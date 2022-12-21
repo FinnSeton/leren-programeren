20f5c6ab234bf51c28f357bbd29faac604296f42
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2022-12-21 12:03:09 +0100

    Create opdracht9.py

---
+++ Module04/Fruit/opdracht9.py
@@ -0,0 +1,12 @@
+from fruitmand import fruitmand
+colors = []
+
+for i in fruitmand:
+    if i['name'] == 'druif':
+        fruitmand.remove(i)
+
+for i in fruitmand:
+    if i['color'] not in colors:
+        colors.append(i['color'])
+
+print(colors)
