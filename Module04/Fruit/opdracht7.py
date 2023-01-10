da9bc385742750183f89009c3e3d068415af02a0
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2023-01-10 09:41:47 +0100

    .

--- Module04/Fruit/opdracht7.py
+++ Module04/Fruit/opdracht7.py
@@ -1,5 +1,4 @@
 from fruitmand import fruitmand
-
 for fruit in fruitmand:
     if fruit["round"]:
         print(fruit["name"])
--- Module04/Fruit/opdracht9.py
+++ Module04/Fruit/opdracht9.py
@@ -1,12 +1,9 @@
 from fruitmand import fruitmand
 colors = []
-
 for i in fruitmand:
     if i['name'] == 'druif':
         fruitmand.remove(i)
-
 for i in fruitmand:
     if i['color'] not in colors:
         colors.append(i['color'])
-
 print(colors)
