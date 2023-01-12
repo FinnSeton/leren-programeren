217c48a8dd8ccb996a0295b88f9bf753eac67db7
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2023-01-12 14:00:20 +0100

    Update lootjesverdeler.py

--- Module04/lootjesverdeler.py
+++ Module04/lootjesverdeler.py
@@ -1,27 +1,26 @@
 namen = []
-aantalnamen = 0
 meernamen = "ja"
-lootjeaantal = 1
+lootjeaantal = 0
 keuze1 = 0
-keuze2 = 1 
+keuze2 = 1
 
 while meernamen == "ja":
-        naaminput = input("Geef een naam: ").capitalize()
-        if naaminput in namen:
-            print("Deze naam is al ingevoerd.")
-        elif naaminput not in namen:
-            namen.append(naaminput)
-            aantalnamen += 1
-            if aantalnamen >= 3:
-                meernamen = input("Wil je nog meer namen invoeren? (ja/nee): ").lower()
+    naaminput = input("Geef een naam: ").capitalize()
+    if naaminput in namen:
+        print("Deze naam is al ingevoerd.")
+    elif naaminput not in namen:
+        namen.append(naaminput)
+        if len(namen) >= 3:
+            while meernamen != 'nee' and 'ja':
+                meernamen = input(
+                    "Wil je nog meer namen invoeren? (ja/nee): ").lower()
 
 try:
-    print(f"Er zijn {aantalnamen} namen ingevoerd.")
-    while lootjeaantal != aantalnamen:
-        print(f"Lootje {lootjeaantal}")
-        print(f"{namen[keuze1]} | {namen[keuze2]}")
+    while lootjeaantal != len(namen):
         lootjeaantal += 1
-        keuze1 += 2
-        keuze2 += 2
+        print(f"Lootje {lootjeaantal}")
+        print(f"{namen[keuze1]} heeft {namen[keuze2]}")
+        keuze1 += 1
+        keuze2 += 1
 except:
-    print(f"Er is 1 iemand over {namen[-1]} ")
+    print(f"{namen[-1]} heeft {namen[0]}")
